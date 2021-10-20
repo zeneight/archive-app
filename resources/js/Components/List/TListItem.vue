6f5b5da1552aaf39f05885109439c323cef8c42d
Agung Priambada <thezeneight@gmail.com>
2021-10-20 10:11:32 +0800

    intialize setup

---
+++ resources/js/Components/List/TListItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="[
+        'flex space-x-2 first:border-t border-b border-l border-r hover:shadow items-center w-full whitespace-normal p-2 transition duration-300 ease-in-out',
+        $slots.secondContent ? 'justify-between' : 'justify-start',
+        calculatedListStyle
+        ]">
+        <!--Icon-->
+        <div v-if="$slots.icon">
+            <slot name="icon"></slot>
+        </div>
+        <!--Title-->
+        <div v-if="$slots.title">
+            <b>
+                <slot name="title"></slot>
+            </b>
+        </div>
+        <!--Default Content-->
+        <div class="flex flex-grow min-w-min">
+            <slot></slot>
+        </div>
+        <!--Second Content-->
+        <div v-if="$slots.secondContent">
+            <slot name="secondContent"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import {listStyleMixin} from "@/Mixins/Styles/listStyleMixin";
+
+export default {
+    name: "TListItem",
+    props: {
+        active: {
+            required: false
+        }
+    },
+    mixins: [listStyleMixin],
+}
+</script>
+
+<style scoped>
+
+</style>
